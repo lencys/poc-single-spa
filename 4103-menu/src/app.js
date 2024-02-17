288a9469c08127412b02bced559c66c27f68c0dc
lencys <lukas.ondrejcek@gmail.com>
2024-02-17 06:43:15 +0100

    Edit menu

--- 4103-menu/src/app.js
+++ 4103-menu/src/app.js
@@ -8,9 +8,7 @@ class Menu extends HTMLElement {
         this.innerHTML = `
             <a href="/react" onclick="singleSpaNavigate(event)">React</a> |
             <a href="/vanilla" onclick="singleSpaNavigate(event)">Vanilla</a> |
-            <a href="/vue" onclick="singleSpaNavigate(event)">Vue</a> |
-            <a href="/wjelements" onclick="singleSpaNavigate(event)">WJ Elements</a> | 
-            <a href="/inventory" onclick="singleSpaNavigate(event)">Inventory</a>
+            <a href="/vue" onclick="singleSpaNavigate(event)">Vue</a>
         `;
     }
 }
