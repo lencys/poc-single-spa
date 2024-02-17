09ab46dc542dadb8ee5f62f1ce50a5923745b1c1
lencys <lukas.ondrejcek@gmail.com>
2024-02-17 06:39:40 +0100

    root

---
+++ 0-root/src/main.ts
@@ -0,0 +1,20 @@
+import { registerApplication, start } from "single-spa";
+import { constructApplications, constructRoutes, constructLayoutEngine } from "single-spa-layout";
+import microfrontendLayout from "./microfrontend-layout.html?raw";
+import './style.css';
+
+const routes = constructRoutes(microfrontendLayout);
+const applications = constructApplications({
+    routes,
+    loadApp({ name }) {
+        return import(/* @vite-ignore */name);
+    },
+});
+
+const layoutEngine = constructLayoutEngine({ routes, applications });
+applications.forEach(registerApplication);
+
+layoutEngine.activate();
+start({
+    urlRerouteOnly: true,
+});
---
+++ 0-root/vite.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import vitePluginSingleSpa from 'vite-plugin-single-spa';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [vitePluginSingleSpa({
+        type: 'root'
+    })],
+})
