abd35defe099d95e59a591362334952b86e84237
Ramirez Vargas, José Pablo <webJose@gmail.com>
2024-02-28 21:25:58 -0600

    fix:  Correct parcels export
    - Corrected dev import map file name.
    - Added ignore of node_modules/.
    - Corrected export of parcels (factory pattern workaround).

--- SvelteParcels/src/parcels.ts
+++ SvelteParcels/src/parcels.ts
@@ -3,14 +3,16 @@ import Welcome from "./lib/Welcome.svelte";
 import singleSpaSvelte from 'single-spa-svelte';
 import { cssLifecycleFactory } from 'vite-plugin-single-spa/ex';
 
-const lc = singleSpaSvelte({
-    component: Welcome
-});
 
-const cssLc = cssLifecycleFactory('spa');
-export const welcomeParcel = {
-    bootstrap: [cssLc.bootstrap, lc.bootstrap],
-    mount: [cssLc.mount, lc.mount],
-    unmount: [cssLc.unmount, lc.unmount],
-    update: lc.update
+const cssLc = cssLifecycleFactory('parcels');
+export function welcomeParcel() {
+    const lc = singleSpaSvelte({
+        component: Welcome
+    });
+    return {
+        bootstrap: [cssLc.bootstrap, lc.bootstrap],
+        mount: [cssLc.mount, lc.mount],
+        unmount: [cssLc.unmount, lc.unmount],
+        update: lc.update
+    }
 };
