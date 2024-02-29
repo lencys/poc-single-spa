59e28d69b859cd2189b8bc9ba51144a4a683c4a2
Ramirez Vargas, José Pablo <webJose@gmail.com>
2024-02-28 21:37:10 -0600

    fix:  Specify correct CSS strategy

--- SvelteParcels/vite.config.ts
+++ SvelteParcels/vite.config.ts
@@ -5,7 +5,8 @@ import vitePluginSingleSpa from 'vite-plugin-single-spa'
 export default defineConfig({
   plugins: [svelte(), vitePluginSingleSpa({
     serverPort: 4202,
-    spaEntryPoints: 'src/parcels.ts'
+    spaEntryPoints: 'src/parcels.ts',
+    cssStrategy: 'multiMife'
   })],
   base: 'http://localhost:4202'
 });
